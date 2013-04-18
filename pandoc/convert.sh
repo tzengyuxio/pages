b525d34eb336ba931bc3eae87eb3c9cb4eea2424
Tzeng Yuxio <tzengyuxio@gmail.com>
2013-04-19 03:52:35 +0800

    add disqus

--- pandoc/convert.sh
+++ pandoc/convert.sh
@@ -2,4 +2,5 @@
 
 pandoc pandoc.markdown -t html5 -o index.html --toc --smart --template=pm-template
 
-sed -i '' 's/<table>/<table class="table table-bordered table-condensed">/' pandoc.html
+# add style to table
+sed -i '' 's/<table>/<table class="table table-bordered table-condensed">/' index.html
